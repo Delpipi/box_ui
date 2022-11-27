9b59f94b224ebfd95bbf696514a6f3aff9a6e564
Delpipi <alexandrepaulkouame@gmail.com>
2022-11-27 22:48:50 +0000

    add box_input_field

--- example/lib/example_view.dart
+++ example/lib/example_view.dart
@@ -1,18 +1,31 @@
 import 'package:box_ui/box_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 class ExampleView extends StatelessWidget {
-  const ExampleView({super.key});
+  ExampleView({super.key});
+
+  
+  final form = FormGroup({
+    'fullName': FormControl<String>(
+      value: 'John Doe',
+      validators: [Validators.required],
+      touched: true,
+    ),
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
-        children: const [
+        children:   [
           BoxText.headingOne('Design System'),
           BoxButton(title: 'SIGN IN'),
-          
+          ReactiveForm(
+            formGroup: form,
+            child: BoxInputField(formControlName: 'fullName',placeholder: 'Nom et Prénom',),
+          )
         ],
       ),
     );
--- lib/box_ui.dart
+++ lib/box_ui.dart
@@ -2,3 +2,4 @@ library box_ui;
 
 export 'src/widgets/box_text.dart';
 export 'src/widgets/box_button.dart';
+export 'src/widgets/box_input_field.dart';

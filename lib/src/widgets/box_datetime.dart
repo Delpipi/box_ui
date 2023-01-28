17ca050d5db644a7abf55ae9ef67bf08832e4f89
Delpipi <alexandrepaulkouame@gmail.com>
2023-01-28 16:32:38 +0000

    update box_datetime.dart

--- lib/src/widgets/box_datetime.dart
+++ lib/src/widgets/box_datetime.dart
@@ -7,22 +7,22 @@ class BoxDateTime extends GetConnect {
   Future<String> getCurrentDate() async {
     var response =
         await get("http://worldtimeapi.org/api/timezone/Africa/Abidjan");
-    var data = jsonDecode(response.body);
-    return Jiffy(DateTime.parse(data['datetime'])).yMMMMEEEEd;
+    Map<String, Object?> data = json.decode(response.body);
+    return Jiffy(DateTime.parse(data['datetime'] as String)).yMMMMEEEEd;
   }
 
   Future<String> getCurrentDateTime() async {
     var response =
         await get("http://worldtimeapi.org/api/timezone/Africa/Abidjan");
-    var data = jsonDecode(response.body);
-    return Jiffy(DateTime.parse(data['datetime'])).yMMMMEEEEdjm;
+    Map<String, Object?> data = jsonDecode(response.body);
+    return Jiffy(DateTime.parse(data['datetime'] as String)).yMMMMEEEEdjm;
   }
 
   Future<String> getCurrentTime() async {
     var response =
         await get("http://worldtimeapi.org/api/timezone/Africa/Abidjan");
-    var data = jsonDecode(response.body);
-    return Jiffy(DateTime.parse(data['datetime'])).Hm;
+    Map<String, Object?> data = jsonDecode(response.body);
+    return Jiffy(DateTime.parse(data['datetime'] as String)).Hm;
   }
 
   Future<int> getDayOfWeek() async {
